140aaa6bae7aae4010083c43d1e57f9df152cbf5
加瀬 正浩 <masahiro_karo@kasenoMacBook-Air.local>
2023-10-19 00:36:56 +0900

    add: components for mui checkbox

---
+++ src/app/_components/mui/Checkbox/index.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Checkbox from '.';
+
+const meta = {
+  component: Checkbox,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ src/app/_components/mui/Checkbox/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Checkbox as MuiCheckbox, CheckboxProps as MuiCheckboxProps } from '@mui/material';
+
+export type CheckboxProps = MuiCheckboxProps;
+
+const Checkbox: React.FC<CheckboxProps> = (props: CheckboxProps) => {
+  return <MuiCheckbox {...props} />;
+};
+
+export default Checkbox;
