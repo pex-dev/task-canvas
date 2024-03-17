63fd9ea85129934ea3c3f10d3f7d283525bb0c34
KARO <masahiro_karo@KAROMacBook-Air.local>
2024-03-17 21:05:41 +0900

    fix: front dev

--- frontend/src/_components/atoms/Checkbox/index.tsx
+++ frontend/src/_components/atoms/Checkbox/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 
 import MuiCheckbox, { CheckboxProps as MuiCheckboxProps } from '../../mui/Checkbox';
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,32 +1,14 @@
-'use client';
+import Auth from '@/_components/organisms/AppAuth';
+import AppHeader from '@/_components/organisms/AppHeader';
 
-import { Auth0Provider } from '@auth0/auth0-react';
-
-import AuthorizedApolloProvider from './_components/AuthorizedApolloProvider';
-import Auth from './_components/organisms/AppAuth';
-import AppHeader from './_components/organisms/AppHeader';
-
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="ja">
-      <body>
-        <Auth0Provider
-          authorizationParams={{
-            redirect_uri: process.env.NEXT_PUBLIC_VERCEL_URL,
-          }}
-          clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
-          domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
-        >
-          <AuthorizedApolloProvider>
-            <Auth>
-              <AppHeader />
-              {children}
-            </Auth>
-          </AuthorizedApolloProvider>
-        </Auth0Provider>
-      </body>
+      <body>{children}</body>
     </html>
   );
-};
-
-export default RootLayout;
+}
