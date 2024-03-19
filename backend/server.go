1b6a9b068c5268d61a5bcb28e13db77e8d27267c
kamegoro <tentyo0620@icloud.com>
2024-03-20 02:56:52 +0900

    init: echo server

--- backend/server.go
+++ backend/server.go
@@ -1,7 +1,21 @@
 package main
 
-const defaultPort = "8080"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+const defaultPort = ":8080"
 
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World")
+	})
+	e.GET("/TODO", func(c echo.Context) error {
+		return c.String(http.StatusOK, "TODOが投げられました")
+	})
 
+	e.Logger.Fatal(e.Start(defaultPort))
 }
