e27c1e4752229d64f7343d203887d3b29203f9eb
KARO <masahiro_karo@KAROMacBook-Air.local>
2024-03-24 00:12:24 +0900

    fix: storybook alias setting

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -16,5 +17,15 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: ['../public'],
+  webpackFinal(config) {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+    return config;
+  },
 };
+
 export default config;
