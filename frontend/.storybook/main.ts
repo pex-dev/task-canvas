01fcd71582344e4956cb9a8657076027394be5d7
kamegoro <tentyo0620@icloud.com>
2024-03-28 23:48:11 +0900

    fix: storybook main.ts

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -3,12 +3,7 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
