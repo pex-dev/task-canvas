c2240efddf8ff487da266690dbd438e7e7a7a25f
KARO250827 <masahiro.k1009@gmail.com>
2024-07-27 16:34:01 +0900

    update: cors settings

--- backend/main.go
+++ backend/main.go
@@ -8,12 +8,18 @@ import (
 	"task-canvas/rest"
 
 	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
 
 const defaultPort = ":8080"
 
 func main() {
 	e := echo.New()
+	e.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
 	middleware.Initlogging(e, logger.Logger)
 
 	e.GET("/", func(c echo.Context) error {
