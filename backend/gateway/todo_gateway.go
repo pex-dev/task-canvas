d1c785aff6c874e4c5b4a76701b54b36283f4c6f
kamegoro <tentyo0620@icloud.com>
2024-07-27 13:11:12 +0900

    add: todo gateway

---
+++ backend/gateway/todo_gateway.go
@@ -0,0 +1,37 @@
+package gateway
+
+import (
+	"context"
+	"task-canvas/domain"
+	db_driver "task-canvas/driver/generated"
+	"task-canvas/port"
+)
+
+type TodoGateway struct {
+	db_driver db_driver.Querier
+}
+
+func NewTodoGateway(db_driver db_driver.Querier) port.TodoPort {
+	return &TodoGateway{
+		db_driver: db_driver,
+	}
+}
+
+func (g *TodoGateway) Get(ctx context.Context) ([]domain.Todo, error) {
+	todos, err := g.db_driver.FindTodo(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]domain.Todo, 0, len(todos))
+
+	for _, todo := range todos {
+		res = append(res, domain.Todo{
+			ID:        todo.ID,
+			Content:   todo.Content,
+			Completed: todo.Completed,
+		})
+	}
+
+	return res, nil
+}
