dadedf68bd07498889364048517fb65c4d1bf795
KARO250827 <masahiro.k1009@gmail.com>
2024-08-11 02:36:56 +0900

    add: driver for POST

--- frontend/src/driver/index.ts
+++ frontend/src/driver/index.ts
@@ -1,16 +1,42 @@
-type Todo = {
+type DriverTodo = {
   id: string;
   content: string;
   completed: boolean;
 };
 
-type Todos = {
-  todos: Todo[];
+type DriverTodos = {
+  todos: DriverTodo[];
 };
 
-export const getTodos = async (): Promise<Todos> => {
+export type CreateDriverRequest = {
+  content: string;
+  completed: boolean;
+};
+
+type CreateTodoResult = string;
+
+export const getTodos = async (): Promise<DriverTodos> => {
   const response = await fetch('http://localhost:8080/v1/todos');
   const json = await response.json();
 
   return json;
+};
+
+export const createTodo = async ({
+  content,
+  completed,
+}: CreateDriverRequest): Promise<CreateTodoResult> => {
+  const response = await fetch('http://localhost:8080/v1/todos', {
+    method: 'POST',
+    body: JSON.stringify({
+      content: content,
+      completed: completed,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const json = await response.json();
+  return json.id;
 };
