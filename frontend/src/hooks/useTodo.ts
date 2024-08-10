1bec679d3a5f46271d6f1f4a5bca146982ecaec8
KARO250827 <masahiro.k1009@gmail.com>
2024-08-11 02:39:03 +0900

    add: hooks for POST

--- frontend/src/hooks/useTodo.ts
+++ frontend/src/hooks/useTodo.ts
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Todo } from '@/domain/todo';
 import { getTodos } from '@/useCase/getTodoUseCase';
+import { createTodo } from '@/useCase/createTodoUseCase';
 
 export const useTodo = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = (content: string) => {
-    let newTodos = todos;
-    newTodos.push({ id: Date.now().toString(), content: content, completed: false });
-    setTodos(newTodos);
+    createTodo(content).then((result) => {
+      setTodos([...todos, result]);
+    });
   };
 
   useEffect(() => {
