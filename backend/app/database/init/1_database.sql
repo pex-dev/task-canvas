c964240f394de8d8bbec24291e954e9e8f9dc382
kamegoro <tentyo0620@icloud.com>
2024-09-21 18:49:48 +0900

    fix: sql

--- backend/app/database/init/1_database.sql
+++ backend/app/database/init/1_database.sql
@@ -6,10 +6,10 @@ CREATE TABLE task_canvas.todo (
 
 CREATE TABLE task_canvas.tag (
     id uuid primary key,
-    name text not null,
+    name text not null
 );
 
 CREATE TABLE task_canvas.todo_tag (
     todo_id uuid references task_canvas.todo(id),
-    tag_id uuid references task_canvas.tag(id),
+    tag_id uuid references task_canvas.tag(id)
 );
