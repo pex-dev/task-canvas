db9aca544718b2f505be0035547ec34f47669cef
kamegoro <tentyo0620@icloud.com>
2024-09-29 01:26:16 +0900

    add user table

--- backend/app/database/init/1_database.sql
+++ backend/app/database/init/1_database.sql
@@ -1,7 +1,18 @@
+CREATE TABLE task_canvas.user (
+    id uuid primary key,
+    email text not null unique,
+    password text
+);
+
 CREATE TABLE task_canvas.todo (
     id uuid primary key,
     content text not null,
     completed boolean not null
+);
+
+CREATE TABLE task_canvas.user_todo (
+    user_id uuid references task_canvas.user(id),
+    todo_id uuid references task_canvas.todo(id)
 );
 
 CREATE TABLE task_canvas.tag (
