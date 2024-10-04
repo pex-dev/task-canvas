3f5e9b064cfc124c67c6db00c94a2aeb1ebe8286
kamegoro <tentyo0620@icloud.com>
2024-10-04 22:35:18 +0900

    add signUp useCase

---
+++ frontend/src/useCase/signUpUseCase.ts
@@ -0,0 +1,8 @@
+import { SignUp } from "@/domain/signUp";
+import SignUpGateway from "@/gateway/signUp";
+
+export const signUp = async ({ email, password }: SignUp): Promise<void> => {
+  const userGateway = new SignUpGateway()
+
+  await userGateway.signUp(email, password)
+}
