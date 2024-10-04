f50dcb054d0f55f7354b69cde6f01ca3100ae6b9
kamegoro <tentyo0620@icloud.com>
2024-10-04 22:33:53 +0900

    fix error components

--- frontend/src/_components/mui/TextField/index.tsx
+++ frontend/src/_components/mui/TextField/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import React, { forwardRef } from 'react';
 import { TextField as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@mui/material';
 
 export type TextFieldProps = MuiTextFieldProps;
 
-const TextFiled: React.FC<TextFieldProps> = (props) => {
-  return <MuiTextField {...props} />;
-};
+const TextFiled = forwardRef<HTMLDivElement, TextFieldProps>((props, ref) => {
+  return <MuiTextField {...props} ref={ref} />;
+});
+
+TextFiled.displayName = 'TextFiled';
 
 export default TextFiled;
