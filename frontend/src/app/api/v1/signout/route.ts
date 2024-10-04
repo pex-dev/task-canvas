9024afcee7531cd85634aab813c5ad0cebf2c0cc
kamegoro <tentyo0620@icloud.com>
2024-10-05 02:32:03 +0900

    update: layout

---
+++ frontend/src/app/api/v1/signout/route.ts
@@ -0,0 +1,10 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const POST = () => {
+  const cookieStore = cookies();
+
+  cookieStore.delete('token');
+
+  return new NextResponse(null, { status: 200 });
+};
