50ca9337433c2619c23741753c3585901a9fb29d
kamegoro <tentyo0620@icloud.com>
2024-10-05 02:30:01 +0900

    add: atom alert

---
+++ frontend/src/_components/atoms/Alert/index.tsx
@@ -0,0 +1,25 @@
+import { Theme, SxProps } from '@mui/material';
+
+import MuiAlert, { AlertPropsType as AlertProps } from '@/_components/mui/Alert';
+
+export type AlertPropsType = Pick<AlertProps, 'severity' | 'children' | 'sx'>;
+
+const Alert = ({ sx, severity, children }: AlertPropsType): JSX.Element => (
+  <MuiAlert
+    sx={
+      [
+        {
+          width: 160,
+          boxShadow:
+            'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+        },
+        ...(Array.isArray(sx) ? (sx as SxProps<Theme>[]) : ([sx] as SxProps<Theme>[])),
+      ] as SxProps<Theme>
+    }
+    severity={severity}
+  >
+    {children}
+  </MuiAlert>
+);
+
+export default Alert;
