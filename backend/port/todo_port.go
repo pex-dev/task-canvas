c92ee594382cad0d286daf13bfffa510cbe7c725
kamegoro <tentyo0620@icloud.com>
2024-10-07 23:03:53 +0900

    update delete todo port

--- backend/port/todo_port.go
+++ backend/port/todo_port.go
@@ -9,5 +9,5 @@ type TodoPort interface {
 	Get(ctx context.Context, userId domain.UserId) ([]domain.Todo, error)
 	Store(ctx context.Context, todo domain.Todo) error
 	Update(ctx context.Context, todo domain.Todo) error
-	Delete(ctx context.Context, id domain.TodoId) error
+	Delete(ctx context.Context, id domain.TodoId, userId domain.UserId) error
 }
