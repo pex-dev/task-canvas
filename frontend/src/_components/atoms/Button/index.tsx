a66509c85b71ebc80e86828e404d4906e158287a
kamegoro <tentyo0620@icloud.com>
2025-01-21 23:04:37 +0900

    update atomButton components for a11y

--- frontend/src/_components/atoms/Button/index.tsx
+++ frontend/src/_components/atoms/Button/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import MuiButton, { ButtonProps as MuiButtonProps } from '../../mui/Button';
 
-export type ButtonProps = Pick<MuiButtonProps, 'children' | 'sx' | 'onClick' | 'disabled'>;
+export type ButtonProps = Pick<MuiButtonProps, 'children' | 'sx' | 'onClick' | 'disabled' | 'type'>;
 
-const Button: React.FC<ButtonProps> = ({ children, sx, onClick, disabled }) => {
+const Button: React.FC<ButtonProps> = ({ children, sx, onClick, disabled, type }) => {
   return (
     <MuiButton
       disabled={disabled}
       onClick={onClick}
       variant="contained"
+      type={type}
       sx={[
         {
           textTransform: 'none',
