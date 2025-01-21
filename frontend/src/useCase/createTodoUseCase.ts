0291eb26bd6f41f0c8a40c8c34a9a20dd31009d4
kamegoro <tentyo0620@icloud.com>
2025-01-21 23:06:23 +0900

    update createTodoUseCase

--- frontend/src/useCase/createTodoUseCase.ts
+++ frontend/src/useCase/createTodoUseCase.ts
@@ -1,12 +1,9 @@
 import { Todo } from '@/domain/todo';
 import TodoGateway from '@/gateway/todo';
 
-export const createTodo = (content: Todo['content']): Promise<Todo> => {
+export const createTodo = async (content: Todo['content']): Promise<Todo> => {
   const todoGateway = new TodoGateway();
-
   const completed = false;
-  const todo = todoGateway.createTodo(content, completed).then((result) => {
-    return result;
-  });
-  return todo;
+
+  return await todoGateway.createTodo(content, completed);
 };
